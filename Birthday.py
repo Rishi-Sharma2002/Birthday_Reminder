41dd33f501fef039eefb6d0bc8cf57324f22258f
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-02-26 10:30:55 +0530

    Update Birthday.py

--- Birthday.py
+++ Birthday.py
@@ -3,13 +3,14 @@ import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-
-
 creds = ServiceAccountCredentials.from_json_keyfile_name('BirthdayReminder.json', scope)
-
-
 client = gspread.authorize(creds)
 
 sheet = client.open('Birthdays').sheet1
 
-print(sheet.col[1])
+
+# print(type(sheet.get_all_records()))
+
+for i in sheet.get_all_records():
+    if(i.Name == "Rishi"):
+        print(i)
